19cfcdc0f818688b17108f497f6702681cb708ba
Calvin Cao <calvincao18@gmail.com>
2019-04-05 16:45:59 -0400

    commit

---
+++ code/scraping/modules/db_connect.py
@@ -0,0 +1,7 @@
+from pymongo import MongoClient
+
+
+def db_connect(db_cred):
+    with open(db_cred, 'r', encoding='utf-8') as fhand:
+        uri, db, col = fhand.read().strip().split('\n')
+        return MongoClient(uri)[db][col]
---
+++ code/scraping/scrape-main.py
@@ -0,0 +1,4 @@
+import os
+from modules.db_connect import *
+
+col = db_connect('../connection/db1.credential')
