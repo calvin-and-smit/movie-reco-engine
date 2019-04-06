a249d80ad9e11b2f375d1237e24ce755d9847dcd
Calvin Cao <calvincao18@gmail.com>
2019-04-06 16:33:26 -0400

    git ignore

--- code/scraping/scrape-main.py
+++ code/scraping/scrape-main.py
@@ -1,5 +1,9 @@
 import os
 from db_connect import *
 
+# Setup working directory to script's location
+os.chdir('/Users/CalvinCao/Resilio Sync/GitHub/reco-engine/code/scraping')
+# os.chdir(os.path.dirname(os.path.realpath(__file__)))
+
 test_db = '../../connection/db1.credential'
 col = db_connect(test_db)
