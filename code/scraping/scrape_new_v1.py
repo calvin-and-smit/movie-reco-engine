f47558f0d700f234adcc0855de63903d838c985c
Smit Mehta <mehta.smit92@gmail.com>
2019-04-07 01:55:52 -0400

    Update scrape_new_v1.py

--- code/scraping/scrape_new_v1.py
+++ code/scraping/scrape_new_v1.py
@@ -33,12 +33,12 @@ def calculate_pages(counts):
     if counts['count'] == 0:
         return 0
     else:
-        return int(counts['total']/32) + 1
+        return math.ceil(counts['total']/32)
     
 
 
 
-pageLink = 'https://www.rottentomatoes.com/api/private/v2.0/browse?maxTomato=100&maxPopcorn=100&services=amazon%3Bhbo_go%3Bitunes%3Bnetflix_iw%3Bvudu%3Bamazon_prime%3Bfandango_now&genres=1&sortBy=release&type=dvd-streaming-all&page=1'
+pageLink = 'https://www.rottentomatoes.com/api/private/v2.0/browse?maxTomato=100&maxPopcorn=100&services=amazon%3Bhbo_go%3Bitunes%3Bnetflix_iw%3Bvudu%3Bamazon_prime%3Bfandango_now&genres=2&sortBy=release&type=dvd-streaming-all&page=1'
 
 
 try:
@@ -54,7 +54,7 @@ counts = data['counts']
 results = data['results']
 
 a = calculate_pages(counts)
-
+a
 
 
 
