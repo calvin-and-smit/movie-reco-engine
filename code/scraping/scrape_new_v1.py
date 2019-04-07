3e9eddf0aac29e152468d37696b92356c2e8e680
Smit Mehta <mehta.smit92@gmail.com>
2019-04-07 01:35:36 -0400

    Update scrape_new_v1.py

--- code/scraping/scrape_new_v1.py
+++ code/scraping/scrape_new_v1.py
@@ -23,6 +23,16 @@ def db_connect():
 
 
 
+pageLink = 'https://www.rottentomatoes.com/api/private/v2.0/browse?maxTomato=100&maxPopcorn=100&services=amazon%3Bhbo_go%3Bitunes%3Bnetflix_iw%3Bvudu%3Bamazon_prime%3Bfandango_now&genres=1&sortBy=release&type=dvd-streaming-all&page=1'
+
+
+try:
+    with urllib.request.urlopen(pageLink) as url1:
+        data=json.loads(url1.read().decode())
+except Exception as e:
+    print('failed attept')
+    time.sleep(2)
+
 
 
 
