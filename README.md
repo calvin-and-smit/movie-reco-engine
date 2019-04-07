12ca0c960341d130b81c21a2903daddd8cf1f083
Calvin Cao <calvincao18@gmail.com>
2019-04-06 21:41:47 -0400

    commit

--- README.md
+++ README.md
@@ -3,6 +3,6 @@
 _WIP...check back later_
 
 Scraping plan:
-- 
+- [Respect RottenTomatoes' robots.txt](https://www.rottentomatoes.com/robots.txt)
 - 
 -
--- code/scraping/scrape-main.py
+++ code/scraping/scrape-main.py
@@ -1,9 +1,7 @@
 import os
-from db_connect import *
+from db_connect import db_connect
 
 # Setup working directory to script's location
-os.chdir('/Users/CalvinCao/Resilio Sync/GitHub/reco-engine/code/scraping')
 # os.chdir(os.path.dirname(os.path.realpath(__file__)))
+os.chdir('code/scraping')
 
-test_db = '../../connection/db1.credential'
-col = db_connect(test_db)
