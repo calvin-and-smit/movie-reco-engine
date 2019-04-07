ec0e60a647f13de9a9afb16b0ae6f65fdabbef25
Smit Mehta <mehta.smit92@gmail.com>
2019-04-07 01:09:34 -0400

    Update scrape_new_v1.py

--- code/scraping/scrape_new_v1.py
+++ code/scraping/scrape_new_v1.py
@@ -10,3 +10,15 @@ from pymongo import errors as pme
 from pymongo import MongoClient
 
 
+def read(file):
+    # Same function as before (read query inputs)
+    with open(file, 'r') as fh:
+        return fh.read().strip().split('\n')
+
+
+def db_connect():
+    with open('db.credential', 'r', encoding='utf-8') as fhand:
+        uri, db, col = fhand.read().strip().split('\n')
+        return MongoClient(uri)[db][col]
+
+
