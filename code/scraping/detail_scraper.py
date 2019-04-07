5e2055262a7b4a74fca1584561b6a84bde3f453c
Calvin Cao <calvincao18@gmail.com>
2019-04-07 18:18:14 -0400

    Create detail_scraper.py

---
+++ code/scraping/detail_scraper.py
@@ -0,0 +1,44 @@
+from bs4 import BeautifulSoup
+from urllib3 import request
+from lxml import etree
+import re
+
+
+def dscraper(url):
+    # Full_URL
+    furl = 'https://www.rottentomatoes.com{}'.format(url)
+    # Load Page
+
+    # Franchise/Flag
+
+    # Movie_Name
+
+    # Critics_Consensus
+
+    # TomatoMeter
+
+    # AudienceScore
+
+    # MI_Description
+
+    # MI_rating
+
+    # MI_Genre
+
+    # MI_Directed_by
+
+    # MI_Written_by
+
+    # MI_In_Theaters
+
+    # MI_On_Disc/Streaming
+
+    # MI_Runtime
+
+    # MI_Studio
+
+    # Watch_It_Now/Streaming_Services
+
+    # Casts(Name+As)
+
+
