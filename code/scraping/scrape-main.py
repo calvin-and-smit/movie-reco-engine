447d0a8e689b24a8c9005610d5a30d57321f969c
Calvin Cao <calvincao18@gmail.com>
2019-04-09 14:46:22 -0400

    Commit

--- code/scraping/scrape-main.py
+++ code/scraping/scrape-main.py
@@ -1,7 +1,8 @@
 import os
 from db_connect import db_connect
+from detail_scraper import dscraper
 
 # Setup working directory
 # os.chdir(os.path.dirname(os.path.realpath(__file__)))
-os.chdir('code/scraping')
+# os.chdir('code/scraping')
 
