1eec968a5b75010cdadb66366560f03331acfcd1
Calvin Cao <calvincao18@gmail.com>
2019-04-11 10:19:34 -0400

    Commit

--- code/scraping/listing_scraper.py
+++ code/scraping/listing_scraper.py
@@ -1,6 +1,5 @@
 from db_connect import db_connect
 from pymongo import errors as pme
-from pprint import pprint
 import requests
 import math
 import time
@@ -26,49 +25,49 @@ def get_existing_urls():
         return list()
 
 
-# Setup working directory
-# os.chdir('code/scraping')
-# os.chdir(os.path.dirname(os.path.realpath(__file__)))
-
-# Read dependency files
-initial_urls = read('../../dependencies/rt_initial_urls')
-genre_codes = read('../../dependencies/rt_genre_codes')
-
-# Load existing urls from db
-existing_urls = get_existing_urls()
-
-# Define other variables
-new_urls = list()
-
-# Record start time
-start_time = time.time()
-
-# Scrape on
-for initial_url in initial_urls:
-    for genre_code in genre_codes:
-        page_num = 1
-        # Knit url
-        url = initial_url.format(genre=genre_code, page=page_num)
-        # Learn page limit
-        max_page = math.ceil(requests.get(url).json()['counts']['total']/32)
-        # Go for launch!
-        while True:
-            if page_num <= max_page:
-                # Knit url, again
-                url = initial_url.format(genre=genre_code, page=page_num)
-                results = requests.get(url).json()['results']
-                # Get movie_urls
-                for item in results:
-                    if item['url'] not in existing_urls+new_urls:
-                        new_urls.append(item['url'])
-                        print(item['url'])
-                page_num += 1
-            else:
-                break
+def lscraper():
+    # Read dependency files
+    initial_urls = read('../../dependencies/rt_initial_urls')
+    genre_codes = read('../../dependencies/rt_genre_codes')
+    # Load existing urls from db
+    existing_urls = get_existing_urls()
+    # Define other variables
+    new_urls = list()
+    # Record start time
+    start_time = time.time()
+    # Scrape on
+    for initial_url in initial_urls:
+        for genre_code in genre_codes:
+            page_num = 1
+            # Knit url
+            i_url = initial_url.format(genre=genre_code, page=1)
+            # Learn page limit
+            max_page = math.ceil(requests.get(i_url).json()['counts']['total']/32)
+            # Go for launch!
+            while True:
+                if page_num <= max_page:
+                    # Knit url, again
+                    url = initial_url.format(genre=genre_code, page=page_num)
+                    results = requests.get(url).json()['results']
+                    # Get movie_urls
+                    for item in results:
+                        if item['url'] not in existing_urls + new_urls:
+                            new_urls.append(item['url'])
+                            print(item['url'])
+                    page_num += 1
+                else:
+                    break
+            continue
         continue
-    continue
+    # Print number of movie_urls captured
+    print('\r\n{} new movie(s) found'.format(len(new_urls)))
+    # Print total run time
+    print('\r\nRun time: {} seconds\r\n'.format(int(time.time() - start_time)))
+    return
 
-# Print number of movie_urls captured
-print(len(new_urls))
-# Print total run time
-print('\r\nRun time: {} seconds\r\n'.format(int(time.time()-start_time)))
+
+if __name__ == '__main__':
+    # Setup working directory
+    # os.chdir('code/scraping')
+    # os.chdir(os.path.dirname(os.path.realpath(__file__)))
+    lscraper()
--- code/scraping/scrape-main.py
+++ code/scraping/scrape-main.py
@@ -1,6 +1,7 @@
 import os
 from db_connect import db_connect
 from detail_scraper import dscraper
+from listing_scraper import lscraper
 
 # Setup working directory
 # os.chdir(os.path.dirname(os.path.realpath(__file__)))
