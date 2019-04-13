d96d5e7ca7d16cacb16a9178ec45d27ba13a2045
Calvin Cao <calvincao18@gmail.com>
2019-04-13 02:53:01 -0400

    Delete db_connect.py

--- code/data-manip/db_connect.py
+++
@@ -1,8 +0,0 @@
-from pymongo import MongoClient
-
-
-def db_connect(db_cred, col_in_use):
-    with open(db_cred, 'r', encoding='utf-8') as fhand:
-        uri, db = fhand.read().strip().split('\n')
-        return MongoClient(uri)[db][col_in_use]
-
