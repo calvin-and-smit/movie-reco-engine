a5e4b8268e867b22cda4cc4699b3c3e934ee9a3a
Smit Mehta <mehta.smit92@gmail.com>
2019-04-13 22:16:12 -0400

    Update data_manip_v1.py

--- code/data-manip/data_manip_v1.py
+++ code/data-manip/data_manip_v1.py
@@ -2,3 +2,18 @@
 #manipulating data
 
 from db_connect import db_connect
+import pandas as pd
+import numpy as np
+
+
+col = db_connect('../../connection-details/db2.credential')
+
+df = pd.DataFrame(list(col.find({}))[:50])
+
+print(df.iloc[0])
+
+print(df.head())
+
+print(df.describe())
+
+print(df.columns)
