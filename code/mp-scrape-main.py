9ab300673b21a30e6fbeaba7c8db6bd001068ca5
Calvin Cao <calvincao18@gmail.com>
2019-04-18 15:17:13 -0400

    Commit

--- code/mp-scrape-main.py
+++ code/mp-scrape-main.py
@@ -7,14 +7,14 @@ from mp_scraper import scrape_with_mp
 
 
 if __name__ == '__main__':
-    # Define global variables
+    # Define variables
     initial_urls = read.by_line('../dependencies/rt_initial_urls')
     genre_codes = read.by_line('../dependencies/rt_genre_codes')
 
     db_credential = ['../connection-details/db-reco-engine.credential',
                      'reco-engine', 'test']
 
-    # Start procedures
+    # Start scraping procedures
     scrape_with_mp(initial_url_list=initial_urls,
                    genre_code_list=genre_codes,
                    db_cred=db_credential,
