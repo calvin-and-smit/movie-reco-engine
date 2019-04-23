517482fd4630c1ccb6d3d30ba585a524fa1d96b1
Calvin Cao <calvincao18@gmail.com>
2019-04-23 18:37:08 -0400

    Commit

--- code/feature-gen/feature_gen_v2.py
+++ code/feature-gen/feature_gen_v2.py
@@ -5,30 +5,17 @@ sys.path.append('../scraping')
 sys.path.append('../tools')
 
 import db_connect
-from copy import deepcopy
 from pprint import pprint
+from copy import deepcopy
 from collections import Counter
 
 
-def feat_gen_directors():
+def feat_gen_directors(director_list: list):
     # Read DB_cred from global
     global db_cred_prod, db_cred_prod
-    # Obtain unique directors as a Counter() object from production collection in the database
-    with db_connect.get_collection(db_cred_prod).find({}) as prod_data:
-        # Obtain unique directors list
-        unique_directors = Counter()
-        for row in prod_data:
-            if 'MI_Director' in row:
-                for director in row['MI_Director']:
-                    unique_directors[director] += 1
-        # Verify length
-        # print(len(unique_directors))
 
-    # Define important directors
-    # (Using the top 20 most common directors extracted from database for now)
-    important_directors = list(item[0] for item in unique_directors.most_common(20))
     # Create data_to_insert template dictionary for faster IO
-    dti_template = dict((f"Dir_{director.replace(' ','-')}", 0) for director in important_directors)
+    dti_template = dict((f"Dir_{director.replace(' ','-')}", 0) for director in director_list)
 
     # Generate Directors feature
     with db_connect.get_collection(db_cred_prod).find({}) as prod_data:
@@ -41,9 +28,7 @@ def feat_gen_directors():
                 for director in row['MI_Director']:
                     if director in important_directors:
                         data_to_insert[f"Dir_{director.replace(' ','-')}"] = 1
-                pass
-            elif 'MI_Director' not in row:
-                pass
+                    continue
             # Insert data into features collection in the database
             # Option upsert=True is necessary; Equivalent to 'insert or update if exists'
             db_connect.get_collection(db_cred_feat).update_one({'_id': row['_id']},
@@ -58,6 +43,7 @@ def feat_gen_directors():
 def feat_gen_genre():
     # Read DB_cred from global
     global db_cred_prod, db_cred_prod
+
     # Load unique genre codes from production dataset
     gen_code_list = set()
     with db_connect.get_collection(db_cred_prod).find({}) as prod_data:
@@ -92,8 +78,20 @@ if __name__ == '__main__':
     db_cred_feat = ['../../connection-details/db-reco-engine.credential',
                     'reco-engine', 'features']
 
+    # Obtain unique directors as a Counter() object from production collection in the database
+    with db_connect.get_collection(db_cred_prod).find({}) as production_data:
+        # Obtain unique directors list
+        unique_directors = Counter()
+        for line in production_data:
+            if 'MI_Director' in line:
+                for dirctr in line['MI_Director']:
+                    unique_directors[dirctr] += 1
+    # Define important directors
+    # (Using the top 20 most common directors extracted from database for now)
+    important_directors = list(item[0] for item in unique_directors.most_common(20))
+
     # Generate director feature
-    feat_gen_directors()
+    feat_gen_directors(director_list=important_directors)
 
     # Generate Genre feature
     feat_gen_genre()
