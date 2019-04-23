ffcd4c440ba5bcaf52a6ac6ec96ccc39110ff538
Calvin Cao <calvincao18@gmail.com>
2019-04-23 19:25:41 -0400

    Commit

--- code/feature-gen/feature_gen_v2.py
+++ code/feature-gen/feature_gen_v2.py
@@ -12,45 +12,40 @@ from collections import Counter
 def feat_gen_directors(director_list: list):
     # Load variables from global
     global prod_data, db_cred_feat
-
     # Check for empty input list
-    if len(director_list) < 1:
-        print('Bad list')
-        return
-
-    # Create data_to_insert template dictionary for faster IO
-    dti_template = dict((f"Dir_{director.replace(' ','-')}", 0) for director in director_list)
-
-    # Generate Directors feature
-    for row in prod_data:
-        # Use Deepcopy to create a 'deep' copy from dti_template
-        # Reference: https://thispointer.com/python-how-to-copy-a-dictionary-shallow-copy-vs-deep-copy/
-        data_to_insert = deepcopy(dti_template)
-        # Check directors one by one
-        if 'MI_Director' in row:
-            for director in row['MI_Director']:
-                if director in important_directors:
-                    data_to_insert[f"Dir_{director.replace(' ','-')}"] = 1
-                continue
-        # Insert data into features collection in the database
-        # Option upsert=True is necessary; Equivalent to 'insert or update if exists'
-        db_connect.get_collection(db_cred_feat).update_one({'_id': row['_id']},
-                                                           {'$set': data_to_insert},
-                                                           upsert=True)
-        continue
+    if len(director_list) > 0:
+        # Create data_to_insert template dictionary for faster IO
+        dti_template = dict((f"Dir_{director.replace(' ','-')}", 0) for director in director_list)
+        # Generate Directors feature
+        for row in prod_data:
+            # Use Deepcopy to create a 'deep' copy from dti_template
+            # Reference: https://thispointer.com/python-how-to-copy-a-dictionary-shallow-copy-vs-deep-copy/
+            data_to_insert = deepcopy(dti_template)
+            # Check directors one by one
+            if 'MI_Director' in row:
+                for director in row['MI_Director']:
+                    if director in important_directors:
+                        data_to_insert[f"Dir_{director.replace(' ','-')}"] = 1
+                    continue
+            # Insert data into features collection in the database
+            # Option upsert=True is necessary; Equivalent to 'insert or update if exists'
+            db_connect.get_collection(db_cred_feat).update_one({'_id': row['_id']},
+                                                               {'$set': data_to_insert},
+                                                               upsert=True)
+    # If input list is empty
+    else:
+        print('Bad director list')
+        pass
     return
 
 
 def feat_gen_genre():
     # Load variables from global
     global prod_data, db_cred_feat
-
     # Load unique genre codes from production dataset
     gen_code_list = set(genre for row in prod_data for genre in row['MI_Genre'])
-
     # Create data_to_insert template
     dti_template = dict((f'Genre_{gen_code}', 0) for gen_code in gen_code_list)
-
     # Generate Genre feature
     for row in prod_data:
         # Check Genre for each movie(row) & insert result into the database
@@ -88,6 +83,7 @@ if __name__ == '__main__':
 
     # Generate director feature
     feat_gen_directors(director_list=important_directors)
-
     # Generate Genre feature
     feat_gen_genre()
+
+    pass
