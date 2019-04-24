19a3988919e520f58851a5392611b4a564c1e674
Smit Mehta <mehta.smit92@gmail.com>
2019-04-24 15:52:34 -0400

    Update feature_gen_v1.py

--- code/feature-gen/feature_gen_v1.py
+++ code/feature-gen/feature_gen_v1.py
@@ -50,9 +50,10 @@ df['MI_Rating'] = df['MI_Rating'].str.replace(r'\s+\(.*\)','')
 # Firstly, replacing the NaNs with empty lists
 df['Casts'].fillna(0, inplace = True)
 
-
-#Firstly, removing the on-screen character names from the list
-
+#Secondly, removing the on-screen character names from the list
+for i in range(len(df['_id'])):
+    if df['Casts'][i] != 0:
+        
 
 
 
