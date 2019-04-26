bfd59ccb99ee01db8e4d839c43a19fd21386838d
Smit Mehta <mehta.smit92@gmail.com>
2019-04-26 15:26:33 -0400

    Update feature_gen_v1.py

--- code/feature-gen/feature_gen_v1.py
+++ code/feature-gen/feature_gen_v1.py
@@ -90,28 +90,4 @@ for i in range(len(df['_id'])):
 ################################################
 
 
-
-################################################
-
-# features to be manipulated:
-## features to be used for content based knn algorithm
-#### Audience Score <S>
-#### Critics Score <S>
-#### Franchise Flag *** <S>
-#### Box Office <S>
-#### Director <X>
-#### Genre **** <X>
-#### Director Actor Pair Flag ****
-#### Actor-Actor Pair Flag
-#### Rating ** <X>
-#### Studio <X>
-#### TomatoMeter <X>
-#### Streaming Service???
-
-## features to be used for content based cosine similarity algorithm
-#### 
-
-## features to be used for collaborative filtering algorithm
-####
-
-################################################
+################################################################################################
